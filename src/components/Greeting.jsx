49abbe192cf9979443b82f5fb59ca8dc7d9dd11c
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-09-09 17:14:03 -0500

    corrected greeting.jsx

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,34 +3,30 @@ import dayImg from "../assets/day.svg";
 import eveningImg from "../assets/evening.svg";
 import nightImg from "../assets/night.svg";
 
-export function Greeting() {
-    const greetingTime = () => {
-		// variable for text and image to displey
-		let img = "";
-		let text = "";
-        
-        if (hour >= 6 && hour < 12) {
-            text = "Good Morning!";
-            img = dayImg;
-          } else if (hour >= 12 && hour < 17) {
-            text = "Good Afternoon!";
-            img = dayImg;
-          } else if (hour >= 17 && hour < 21) {
-            text = "Good Evening!";
-            img = eveningImg;
-          } else {
-            text = "Good Night!";
-            img = nightImg;
-          }
-	};
+function Greeting() {
+  const hour = new Date().getHours();
+  let img = "";
+  let text = "";
 
-    const { img, text } = greetingTime();
-	// return our greeting with the approriate message and image
-	return (
-		<h1 className="greeting">
-			<img src={imgSrc} alt="Time of day" /> {textSrc}
-		</h1>
-	);
+  if (hour >= 6 && hour < 12) {
+    text = "Good Morning!";
+    img = dayImg;
+  } else if (hour >= 12 && hour < 17) {
+    text = "Good Afternoon!";
+    img = dayImg;
+  } else if (hour >= 17 && hour < 21) {
+    text = "Good Evening!";
+    img = eveningImg;
+  } else {
+    text = "Good Night!";
+    img = nightImg;
+  }
+
+  return (
+    <h1 className="greeting">
+      <img src={img} alt={text} /> {text}
+    </h1>
+  );
 }
 
 export default Greeting;
